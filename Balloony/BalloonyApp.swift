1e51e237c898774113d8699a88b3e25899427e4c
Daryl <darylgialolo@gmail.com>
2022-05-25 18:35:56 +0800

    Initial Commit

---
+++ Balloony/BalloonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalloonyApp.swift
+//  Balloony
+//
+//  Created by Wind Versi on 25/5/22.
+//
+
+import SwiftUI
+
+@main
+struct BalloonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
